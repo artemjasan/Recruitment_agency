e3a38486c6da9780be5b6499561b206f85bc521d
AJ <artjasan@gmail.com>
2021-05-31 09:34:35 +0200

    update urls, remove some things

--- agency_project/core/urls.py
+++ agency_project/core/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
-
-SCHEMA_NAME = "api-schema"
 
 api_v1_patterns = [
     path('', include('agency_app.urls')),
@@ -11,9 +8,4 @@ api_v1_patterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_v1_patterns)),
-    path('schema-api/', get_schema_view(
-        title="Rec API Scheme",
-        description="API for all things … and other",
-        version="0.0.0"
-    ), name=SCHEMA_NAME),
 ]
